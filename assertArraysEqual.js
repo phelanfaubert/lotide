ac5f961bb4f724c2451e7d18bc8128fe77ad3fef
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-09 22:26:14 +0000

    Modified array names

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,3 +9,11 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 };
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
