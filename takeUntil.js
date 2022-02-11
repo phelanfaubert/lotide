fc86faaa4bb8f737929e2292b6b540e856f52f00
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-11 01:36:17 +0000

    Added takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const takeUntil = function(array, callback) {
+  let arr = [];
+for (const element of array) {
+  if (callback(element)) {
+    return arr;
+  } else { 
+    arr.push(element)
+  }
+  }
+  return arr;
+}
+
+
+
+  const data1 = [1, 2, 5, 7, 2, 1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
