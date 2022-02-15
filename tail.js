ae6e8f5f4adcd4766582690c40aefeca34f6fd7d
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-15 03:15:53 +0000

    Removed the test cases

--- tail.js
+++ tail.js
@@ -1,16 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   let value = arr.slice(1);
+  console.log(value)
   return value;
+ 
+
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
+
