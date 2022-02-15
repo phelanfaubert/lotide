462a07adb823cebc4b9d5f19834af7483a7d678f
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-15 22:48:13 +0000

    Created index.js for lotide

---
+++ index.js
@@ -0,0 +1,29 @@
+  const head   = require('./head');
+  const tail   = require('./tail');
+  const middle = require('./middle');
+  const countLetters = require('./countLetters')
+  const countOnly = require('./countOnly')
+  const eqArrays = require('./eqArrays')
+  const eqObjects = require('./eqObjects')
+  const findKeyByValue = require('./findKeyByValue')
+  const findKey = require('./findKey')
+  const letterPositions = require('./letterPositions')
+  const map = require('./map')
+  const takeUntil = require('./takeUntil')
+  const without = require('./without')
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKeyByValue,
+  findKey,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+};
