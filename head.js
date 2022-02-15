ca8cb3b9120fe4bf3dd25665c295b58f8415ec31
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-15 02:39:03 +0000

    Removed test cases from head.js file

--- head.js
+++ head.js
@@ -1,13 +1,17 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
 return arr[0]
 }
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
---
+++ test/headTest.js
@@ -0,0 +1,6 @@
+const head = require('../head');
+
+console.log(head([5,6,7]), 5);
+console.log(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
