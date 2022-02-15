dcc04dc71b8f66b745900a1d120ee837fbf28cde
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-15 06:17:40 +0000

    Refactored the strict.equal

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,13 @@
-const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
+//const assertArraysEqual = require('../assertArraysEqual.js');
 const middle = require('../middle.js');
 
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+it("returns [] for [5]", () => {
+  assert.deepEqual(middle([5]), []); 
+  });
+});
