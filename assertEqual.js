676a96ab696a2f7b3e3155c003771bbcecd71713
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-15 02:35:46 +0000

    Removed test cases from assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,13 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅Assertion Passed:" + actual + " === " + expected);
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("❌Assertion Failed:" + actual + " !== " + expected);
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
+module.exports = assertEqual;
 
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+// console.log(assertEqual(1, 1));
