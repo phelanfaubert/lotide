839b825b1e6b7a4cc1ed56873f975bc3c5623c22
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-15 03:56:23 +0000

    Added chair to tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,12 @@
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
 
-console.log(tail([1, 2, 3, 4]))
+it("returns [] for ['5']", () => {
+  assert.deepEqual(tail(['5']), []); 
+});
