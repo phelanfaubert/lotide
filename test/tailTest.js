c64a721289106ac201e06bf240791c10c4dd89e8
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-15 06:18:08 +0000

    Moved curly braces

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,8 +5,9 @@ describe("#tail", () => {
   it("returns [2, 3] for [1, 2, 3]", () => {
     assert.deepEqual(tail([1, 2, 3]), [2, 3]);
   });
-});
 
-it("returns [] for ['5']", () => {
+
+  it("returns [] for ['5']", () => {
   assert.deepEqual(tail(['5']), []); 
+  })
 });
