ef853bda030929626ff8db0197d042dbab63c332
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-15 03:31:03 +0000

    Removed test cases from eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -19,8 +10,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3,4]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false);
+module.exports = eqArrays;
